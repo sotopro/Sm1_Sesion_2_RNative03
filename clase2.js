001db7f65ae5152c851906cdeb9021e29dbdee89
Daniel Soto <dasg.1993@gmail.com>
2021-05-22 09:50:57 -0500

    for looping objects

--- clase2.js
+++ clase2.js
@@ -21,3 +21,21 @@ let isEnable = true;
 let result = !isEnable ? 'weapon' : 'race';
 console.log('gimli weapon', gimli.weapon);
 console.log(gimli[result]);
+console.log('function', gimli.greet());
+// gimli.weapon = 'sword'
+// gimli['race'] = 'elf'
+gimli.fight = function() {
+    return `Gimli attacks with an ${this.weapon}`;
+}
+gimli.parents = false;
+
+console.log('gimli.fight', gimli.fight());
+
+delete gimli.parents;
+
+// Iterate through properties of gimli
+for (let key in gimli) {
+    console.log(`${key}:`, gimli[key]);
+}
+
+console.log('gimli', gimli);
